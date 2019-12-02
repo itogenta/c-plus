852ad540ed9da4490a24ff5741d54927f038bdb4
Itou Genta <b1545276@planet.kanazawa-it.ac.jp>
2019-12-02 21:33:42 +0900

    Initial Commit

---
+++ c++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c++
+//
+//  Created by 伊藤源太 on 2019/12/02.
+//  Copyright © 2019 伊藤源太. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
