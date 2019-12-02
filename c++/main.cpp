de7157baea74dcb6979b9a46aefc7c6a54d927a2
Itou Genta <b1545276@planet.kanazawa-it.ac.jp>
2019-12-02 21:56:50 +0900

    編集しました

--- c++/main.cpp
+++ c++/main.cpp
@@ -8,8 +8,17 @@
 
 #include <iostream>
 
+using namespace std;//名前空間
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    string s,t;
+    t="input";
+    cout<<"input";
+    cin>>s;
+    cout<<t+s<<"desu"<<endl;
+    int a;
+    cin >> a;
+    std::cout <<"a="<<a<<endl ;
     return 0;
 }
